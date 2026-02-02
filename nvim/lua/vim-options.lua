6e325884781196097b68a89747e4bdedf919ab19
Adam P <adam.porbanderwalla@gmail.com>
2026-02-02 11:49:04 -0500

    add: Started working with neovim, these are the current settings and plugins that I am using

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,8 @@
+
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set autoindent")
+vim.cmd("set mouse=a")
+vim.g.mapleader = " " 
