d3f56a2dbe2f709942ad632823fca38f08e9dcaa
Adam P <adam.porbanderwalla@gmail.com>
2026-02-14 19:22:56 -0500

    feat: updated sketchybar to cooler rosepine aesthetics

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -1,46 +1,21 @@
 #!/bin/bash
 
-export WHITE=0xffffffff
+# This is the color of the icons & text on screen, currently I have set it to the rose pine foam
+export WHITE=0xFF9CCFD8
+
+# -- Actual white 
+# export WHITE=0xffffffff
+
+# -- Rose Pine Color Scheme --
+# Bar is transparent @ this hex
+export BAR_COLOR=0x00000000
+export ITEM_BG_COLOR=0xff1e1e2e
+export ACCENT_COLOR=0xFF9CCFD8
+export CONTRAST_COLOR=0xFF9CCFD8
 
 # -- Catppuccin Sapphire Scheme --
-export BAR_COLOR=0x00000000
-export ITEM_BG_COLOR=0xff8d98b8
-export ACCENT_COLOR=0xff74c7ec
-export CONTRAST_COLOR=0xff1e1e2e
-
-# -- Gray Scheme --
-# export BAR_COLOR=0xff101314
-# export ITEM_BG_COLOR=0xff353c3f
-# export ACCENT_COLOR=0xffffffff
-
-# -- Purple Scheme --
-# export BAR_COLOR=0xff140c42
-# export ITEM_BG_COLOR=0xff2b1c84
-# export ACCENT_COLOR=0xffeb46f9
-
-# -- Red Scheme ---
-# export BAR_COLOR=0xff23090e
-# export ITEM_BG_COLOR=0xff591221
-# export ACCENT_COLOR=0xffff2453
-
-# -- Blue Scheme ---
-# export BAR_COLOR=0xff021254
-# export ITEM_BG_COLOR=0xff093aa8
-# export ACCENT_COLOR=0xff15bdf9
-
-# -- Green Scheme --
-# export BAR_COLOR=0xff003315
-# export ITEM_BG_COLOR=0xff008c39
-# export ACCENT_COLOR=0xff1dfca1
-
-
-# -- Orange Scheme --
-# export BAR_COLOR=0xff381c02
-# export ITEM_BG_COLOR=0xff99440a
-# export ACCENT_COLOR=0xfff97716
-
-# -- Yellow Scheme --
-# export BAR_COLOR=0xff2d2b02
-# export ITEM_BG_COLOR=0xff8e7e0a
-# export ACCENT_COLOR=0xfff7fc17
+# export BAR_COLOR=0x00000000
+# export ITEM_BG_COLOR=0xff8d98b8
+# export ACCENT_COLOR=0xff74c7ec
+# export CONTRAST_COLOR=0xff1e1e2e
 
